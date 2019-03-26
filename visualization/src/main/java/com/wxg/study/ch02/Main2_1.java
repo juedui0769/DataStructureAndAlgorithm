ffbab71c2019f153404c51c778ae6c001946ce02
juedui0769 <chixin0769@163.com>
2019-03-26 18:14:47 +0800

    Main2_1 java swing 基础

---
+++ visualization/src/main/java/com/wxg/study/ch02/Main2_1.java
@@ -0,0 +1,18 @@
+package com.wxg.study.ch02;
+
+import javax.swing.*;
+
+/**
+ * 2019年3月26日18:09:02 <p></p>
+ * <a href="https://github.com/liuyubobobo/Play-with-Algorithm-Visualization/tree/master/02-Java-Swing-Basics">02-Java-Swing-Basics</a>
+ */
+public class Main2_1 {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Welcome");
+        frame.setSize(500, 500);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
